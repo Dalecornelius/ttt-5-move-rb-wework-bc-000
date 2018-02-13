b416f2743696fa69c8816968f1c56f55037decfe
dale cornelius <dale.cornelius@wework.com>
2018-02-13 04:57:41 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,6 +8,8 @@ end
 
 # code your input_to_index and move method h
 
-def input_to_index(user_input)
-  input_to_index == user_input.to_i 
+def input_to_index(user_input, converted_input)
+  converted_input = user_input.to_i
+  puts converted_input -1
+  
 end
